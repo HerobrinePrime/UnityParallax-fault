1bac28bbd3e8712d6d41385ae0c20d9dfaf4ce10
herobrine <1193875164@qq.com>
2025-06-03 11:47:31 +0900

    class created

--- Assets/Scripts/Utils/PlayerSettings.cs
+++ Assets/Scripts/Utils/PlayerSettings.cs
@@ -1,9 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace DefaultNamespace.Utils
 {
+    [Serializable]
     public class PlayerSettings
     {
+        private const string Key = "PLAYER_SETTINGS";
+        
+        [SerializeField] private bool reverse;
+        [SerializeField] private float volume;
+        [SerializeField] private bool muted;
+        [SerializeField] private float menuTransparency;
+        [SerializeField] private float xConstraint;
+        [SerializeField] private float yConstraint;
+        
+        
         private static PlayerSettings _instance;
 
         public static PlayerSettings Instance
@@ -14,6 +26,7 @@ namespace DefaultNamespace.Utils
                 {
                     _instance = new PlayerSettings();
                 }
+                return _instance;
             }
         }
     }
