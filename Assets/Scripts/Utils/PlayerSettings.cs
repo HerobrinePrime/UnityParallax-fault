6f5a4add0a57a02a1f2a49b8694bb465a020dd3c
herobrine <1193875164@qq.com>
2025-06-03 11:11:11 +0900

    start playerprefs

---
+++ Assets/Scripts/Utils/PlayerSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.Utils
+{
+    public class PlayerSettings
+    {
+        private static PlayerSettings _instance;
+
+        public static PlayerSettings Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new PlayerSettings();
+                }
+            }
+        }
+    }
+}
